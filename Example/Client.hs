5d452b31634e338fa110c14507ab52c0975ac839
jbarratt <jbarratt@jbarratt.(none)>
2014-02-07 09:36:11 -0600

    Lint

--- Example/Client.hs
+++ Example/Client.hs
@@ -12,10 +12,10 @@ stripValid :: Checked Validated String -> String
 stripValid = drop 7 . uncheck
 
 t1 :: Maybe Integer
-t1 = halfOrThree <$> (check (isEven |> three) 3)
+t1 = halfOrThree <$> check (isEven |> three) 3
 
 t2 :: Maybe Integer
-t2 = halfOrThree <$> (check (isEven |> three) 6)
+t2 = halfOrThree <$> check (isEven |> three) 6
 
 t3 :: Maybe Integer
-t3 = halfOrThree <$> (check (isEven |> three) 7)
+t3 = halfOrThree <$> check (isEven |> three) 7
